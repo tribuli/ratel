dd3a6ac610cc190cbd37d3deba60619f005213a1
guhongzheng <tribuli@163.com>
2017-07-31 21:21:59 +0800

    ratel project init

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1501504754528_6195';
+
+  // add your config here
+
+  return config;
+};
